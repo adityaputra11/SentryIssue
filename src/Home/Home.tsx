bc4dafdb9b0a22360d652d5202a628f910ef3eff
FLP-9-AdityaPratama <adityaputrapratama39@gmail.com.com>
2022-11-03 13:14:34 +0700

    add render transaction

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,15 +1,24 @@
 import React, {useEffect} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, TouchableOpacity, Button} from 'react-native';
 import {fakeFetch} from '../actions/transaction';
 import {withProfiler} from '@sentry/react-native';
+import {useNavigation} from '@react-navigation/native';
 
 const Home = () => {
+  const navigation = useNavigation();
   useEffect(() => {
     fakeFetch();
   }, []);
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Home Screen</Text>
+      <Button
+        title="To List Screen"
+        onPress={() => {
+          navigation.navigate('List');
+          console.log('pressed');
+        }}
+      />
     </View>
   );
 };
--- src/List/List.tsx
+++ src/List/List.tsx
@@ -4,7 +4,7 @@ import {View, Text} from 'react-native';
 const List = () => {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
+      <Text>List Screen</Text>
     </View>
   );
 };
